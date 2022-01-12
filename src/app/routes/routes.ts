982053072afb54c78aec1d26c0af7c7004190518
roma-bashtevych <roma_bashtevich@ukr.net>
2022-01-12 13:36:13 +0200

    lesson 3

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -2,9 +2,10 @@ import {Routes} from "@angular/router";
 import {UsersComponent} from "../components/users/users.component";
 import {UserDetailsComponent} from "../components/user-details/user-details.component";
 import {UserDeactivatorService} from "../services/user-deactivator.service";
+import {UserActivatorService} from "../services/user-activator.service";
 
 export let routes: Routes = [
   {path: '', component: UsersComponent, canDeactivate: [UserDeactivatorService]},
-  {path: 'users/:id', component: UserDetailsComponent, canActivate: []},
+  {path: 'users/:id', component: UserDetailsComponent, canActivate: [UserActivatorService]},
   {path: 'users', redirectTo: '', pathMatch: 'full'},
 ]
